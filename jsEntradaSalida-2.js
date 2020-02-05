f3461cda075a698914755db335c156da884ffa56
markoos98 <60697970+markoos98@users.noreply.github.com>
2020-02-05 12:24:13 -0300

    Add files via upload

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,10 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+var vnom;
+vnom= prompt("Nombre", "Ingrese su Nombre");
+alert (vnom);
+
+}
+
